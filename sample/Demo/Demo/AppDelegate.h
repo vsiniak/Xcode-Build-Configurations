7ac3b1d94c4fb62027653b76e6542a3812209d48
drif <drif@drifs-MacBook-Pro.local>
2015-09-16 19:14:26 +0300

    initial commit

---
+++ sample/Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by drif on 9/16/15.
+//  Copyright (c) 2015 drif LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sample/Demo/Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by drif on 9/16/15.
+//  Copyright (c) 2015 drif LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
